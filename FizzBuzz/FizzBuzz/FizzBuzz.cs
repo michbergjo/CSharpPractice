dca8a197f30df60d72d46e9bbb0b5b62c91355a3
Michelle Joseph <mbergy27@gmail.com>
2021-11-09 09:11:30 -0600

    FizzBuzz in C#

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first value: ");
+            int firstVal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second value: ");
+            int secondVal = Convert.ToInt32(Console.ReadLine());
+
+            FizzBuzzMethod(firstVal, secondVal);
+        }
+        public static void FizzBuzzMethod(int firstVal, int secondVal)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % firstVal == 0 && i % secondVal == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else if (i % firstVal == 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % secondVal == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+
+}
+   
