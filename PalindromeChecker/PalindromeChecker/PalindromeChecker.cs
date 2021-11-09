9d47de73354df133b458310a295983d40ea609a4
Michelle Joseph <mbergy27@gmail.com>
2021-11-09 08:55:46 -0600

    Palindrome check practice in C#

---
+++ PalindromeChecker/PalindromeChecker/PalindromeChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker
+{
+    class PalindromeChecker
+    {
+        public static string reverse(string input)
+        {
+            string output = "";
+
+            if(input == null)
+            {
+                return "Invalid Input";
+            }
+
+            for(int i=input.Length-1; i >= 0; i--)
+            {
+                output += input[i].ToString();
+            }
+
+            return output;
+        }
+
+        public static bool CheckPalindrome(string input, string output)
+        {
+            if(input == output)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PalindromeChecker/PalindromeChecker/TestPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PalindromeChecker;
+
+namespace PalindromeChecker
+{
+    class TestPalindrome
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter an input string: ");
+
+            string input = Console.ReadLine();
+
+            bool isPalindrome = PalindromeChecker.CheckPalindrome(input, PalindromeChecker.reverse(input));
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("{0} is a Palindrome. It is {0} backwards and forwards", input);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Palindrome. It is {0} forwards and {1} backwards",
+                    input, PalindromeChecker.reverse(input));
+            }
+        }
+    }
+}
